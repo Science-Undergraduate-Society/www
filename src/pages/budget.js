c4d55edb9800d2313f70570881f49f98b263ef6e
sahibrao <sahibbbbbb15@gmail.com>
2024-07-29 17:03:47 -0700

    test

--- src/pages/budget.js
+++ src/pages/budget.js
@@ -1,44 +1,46 @@
 import styled from "styled-components";
-import Link from 'next/link';
+import Link from "next/link";
+import Navbar from "../../components/Navbar";
 
 export default function Budget() {
-    return (
-        <Background>
-            <Title>Budget Information page</Title>
-            <Link href="/" passHref>
-                <StyledButton>Back to Home</StyledButton>
-            </Link>
-        </Background>
-    )
+  return (
+    <Background>
+      <Navbar />
+      <Title>Budget Information page</Title>
+      <Link href="/" passHref>
+        <StyledButton>Back to Home</StyledButton>
+      </Link>
+    </Background>
+  );
 }
 
 const Title = styled.div`
-    color: black;
-`
+  color: black;
+`;
 
 const Background = styled.div`
-    height: 100vh;
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-`
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
 const StyledButton = styled.div`
-    display: inline-block;
-    width: 210px;
-    padding: 10px 20px;
-    background-color: #0070f3;
-    color: white;
-    text-align: center;
-    border-radius: 5px;
-    text-decoration: none;
-    margin-top: 20px;
-    cursor: pointer;
+  display: inline-block;
+  width: 210px;
+  padding: 10px 20px;
+  background-color: #0070f3;
+  color: white;
+  text-align: center;
+  border-radius: 5px;
+  text-decoration: none;
+  margin-top: 20px;
+  cursor: pointer;
 
-    &:hover {
-        background-color: #005bb5;
-    }
-`
+  &:hover {
+    background-color: #005bb5;
+  }
+`;
