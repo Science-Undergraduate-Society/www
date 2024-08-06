065c46d187a18453c77e4a27dc11a9f0d4dad141
abbyg85 <abbyg85@student.ubc.ca>
2024-08-05 20:35:21 -0700

    heading styles

--- src/pages/grants.js
+++ src/pages/grants.js
@@ -1,19 +1,50 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
 import Link from 'next/link';
+import Navbar from "../../components/Navbar";
 
 export default function Grants() {
     return (
         <Background>
-            <Title>Grants Information page</Title>
-            <Link href="/" passHref>
-                <StyledButton>Back to Home</StyledButton>
-            </Link>
+            <Navbar></Navbar>
+            <Heading>
+                <Title>SUS Grants & Subsidies</Title>
+                <Subinfo>$53,000 for individuals and clubs</Subinfo>
+                <Link href="">
+                    <StyledButton>Apply Now</StyledButton>
+                </Link>
+            </Heading>
         </Background>
     )
 }
 
+const GlobalStyle = createGlobalStyle`
+    :root {
+    --susblue: #222755;
+    --logoblue: #0173be;
+    --greyblue: #e7edf7;
+    --black: #000000;
+    --white: #ffffff;
+
+}`;
+
+const Heading = styled.div`
+    background-size: cover;
+    width: 100wh;
+    height: 100vh; 
+    padding-top: 150px;
+`
 const Title = styled.div`
-    color: black;
+    color: white;
+    font-size: 54px;
+    font-weight: 700;
+    position: relative;
+`
+
+const Subinfo = styled.div`
+    font-size: 36px;
+    font-weight: 400;
+    color: white;
+    margin-top: 24px;
 `
 
 const Background = styled.div`
@@ -21,17 +52,18 @@ const Background = styled.div`
     background-size: cover;
     display: flex;
     flex-direction: column;
+    text-align: center;
     align-items: center;
     justify-content: center;
-    text-align: center;
+    background-color: #222755;
 `
 
 const StyledButton = styled.div`
     display: inline-block;
     width: 210px;
     padding: 10px 20px;
-    background-color: #0070f3;
-    color: white;
+    background-color: #e7edf7;
+    color: #222755;
     text-align: center;
     border-radius: 5px;
     text-decoration: none;
@@ -42,3 +74,20 @@ const StyledButton = styled.div`
         background-color: #005bb5;
     }
 `
+
+const pulse = keyframes`
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.05);
+    }
+    100% {
+        transform: scale(1);
+    }
+`
+
+const breakpoints = {
+    mobile: '768px',
+    tablet: '1024px',
+};
