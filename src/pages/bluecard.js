c093b863ed6effc8e2b0693a0422b198dfe771b9
abbyg85 <abbyg85@student.ubc.ca>
2024-08-07 20:38:29 -0700

    add partners and eligibility sections

--- src/pages/bluecard.js
+++ src/pages/bluecard.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Link from 'next/link';
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer"
 
 export default function BlueCard() {
     return (
@@ -10,9 +11,12 @@ export default function BlueCard() {
                 <Title>The Blue Card</Title>
                 <SubInfo>The UBC Student Discount Card</SubInfo>
                 <SecondaryMenu>
-                    <SmItem>Blue Card</SmItem>
-                    <SmItem>Eligibility</SmItem>
-                    <SmItem>Partners</SmItem>
+                    <StyledLink href="#eligibility">
+                        <SmItem>Eligibility</SmItem>
+                    </StyledLink>
+                    <StyledLink href="#partners">
+                        <SmItem>Partners</SmItem>
+                    </StyledLink>
                 </SecondaryMenu>
                 <Link href="">
                     <StyledButton>Sign In/Up</StyledButton>
@@ -20,21 +24,44 @@ export default function BlueCard() {
             </Heading>
             <BCInfo>
                 <Title>What is Blue Card?</Title>
+                <InfoP>
+                    Welcome to Bluecard -- Your FREE Virtual Discount Program for all UBC Science Students! 
+                    Discover exclusive benefits at local restaurants and shops by simply presenting your Blue Card.
+                    To get started, follow the register link at the top of the page. 
+                </InfoP>
+                <InfoP>
+                    Submit specific requests for places you want to enjoy discounts using the feedback function at the bottom of our page! 
+                    Our SUS student benefits team will do our best to secure the desired discount for you.
+                </InfoP>
             </BCInfo>
+            <Eligibility>
+                <StyledLink id="eligibility"></StyledLink>
+                <Title>Eligibility</Title>
+                <All>All UBC students in the Faculty of Science are eligible for the Blue Card.</All>
+            </Eligibility>
+            <Partners>
+                <StyledLink id="partners"></StyledLink>
+                <Title>Our Partners</Title>
+                <PartnersBox>
+                    <IndivPartner></IndivPartner>
+                </PartnersBox>
+            </Partners>
+            <Footer></Footer>
         </Background>
     )
 }
 
 const Heading = styled.div`
-    padding-top: 150px;
+    padding-top: 126px;
     color: white;
 `
 const Title = styled.div`
     font-size: 54px;
     font-weight: 700;
     position: relative;
-    letter-spacing: 3px;
+    letter-spacing: 5px;
     text-transform: uppercase;
+    margin-top: 24px;
 `
 
 const SubInfo = styled.div`
@@ -44,6 +71,7 @@ const SubInfo = styled.div`
     letter-spacing: 4px;
     font-style: italic;
 `
+
 const SecondaryMenu = styled.div`
     display: flex;
     flex-direction: row;
@@ -61,6 +89,7 @@ const BCInfo = styled.div`
     width: 100%;
     height: auto;
     margin-top: 24px;
+    padding: 24px;
 `
 
 const SmItem = styled.div`
@@ -68,8 +97,51 @@ const SmItem = styled.div`
     text-align: center;
 `
 
+const InfoP = styled.div `
+    font-size: 18px;
+    color: #222755;
+    text-align: left;
+    width: 70%;
+    margin-left: 15%;
+    padding: 24px;
+`
+
+const Eligibility = styled.div`
+    color: white;
+    width: 100%;
+    height: 100%;
+    padding: 24px;
+`
+
+const All = styled.div`
+    font-size: 36px;
+    color: white; 
+    letter-spacing: 3px;
+    width: 70%;
+    margin-left: 15%;
+    margin-top: 24px;
+    font-weight: 300;
+`
+
+const Partners = styled.div`
+    background-color: #e7edf7;
+    color: #222755;
+    width: 100%;
+    height: auto;
+    margin-top: 24px;
+    padding: 24px;
+`
+
+const PartnersBox = styled.div`
+    display = flex;
+`
+
+const IndivPartner = styled.div`
+    color: #222755;
+`
+
 const Background = styled.div`
-    height: 100vh;
+    height: 100%;
     background-size: cover;
     display: flex;
     flex-direction: column;
@@ -93,4 +165,9 @@ const StyledButton = styled.div`
     &:hover {
         background-color: #005bb5;
     }
+`
+
+const StyledLink = styled.a`
+    text-decoration: none;
+    color: white;
 `
