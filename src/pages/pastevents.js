98fb7c19b8b805c0c3fb0046b3eb466888c8caff
graceco22 <graceco921@gmail.com>
2024-08-09 22:55:04 -0700

    added past events

--- src/pages/pastevents.js
+++ src/pages/pastevents.js
@@ -1,10 +1,21 @@
-import Link from 'next/link';
 import Navbar from "../../components/Navbar";
+import grad from "../../components/event-assets/grad.png";
 
 export default function Events() {
-    return (
-        <div> 
-            <Navbar/>
+  return (
+    <div>
+         <Navbar />
+    
+        <div className="background-container">
+            <div className="overlay-text"><b>Our Past Events</b></div>
         </div>
-    )
+    
+        <div className='subtitle-container'>
+            <hr className='line' />
+            <h2 className='event-subtitle'>Science RXN</h2>
+            <hr className='line' />
+        </div>
+
+    </div>
+  );
 }
