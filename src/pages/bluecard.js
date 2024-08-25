0c73f1cc31fbad45bdaff461bd53fd3b690e2e91
abbyg85 <abbyg85@student.ubc.ca>
2024-08-24 21:40:05 -0700

    final edits/changes

--- src/pages/bluecard.js
+++ src/pages/bluecard.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 import Link from 'next/link';
 import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer"
+import Footer from "../../components/Footer";
 
 export default function BlueCard() {
     return (
         <Background>
             <Navbar></Navbar>
             <Heading>
-                <Title>The Blue Card</Title>
+                <MainTitle>The Blue Card</MainTitle>
                 <SubInfo>The UBC Student Discount Card</SubInfo>
                 <SecondaryMenu>
                     <TopLink href="#eligibility">
@@ -18,121 +18,106 @@ export default function BlueCard() {
                         <SmItem>Partners</SmItem>
                     </TopLink>
                 </SecondaryMenu>
-                <Link href="">
+                <Link href="https://susbluecard.firebaseapp.com/auth">
                     <StyledButton>Sign In/Up</StyledButton>
                 </Link>
             </Heading>
             <MainInfo>
-                <BCInfo>
-                    <Left>
-                        <Title>What is Blue Card?</Title>
-                        <InfoP>
-                            Welcome to Bluecard -- Your FREE Virtual Discount Program for all UBC Science Students! 
+                <ImgSec>
+                    <ImgBackground src="/images/bluecard-images/eligibility.jpg" alt="Logo" />
+                    <BCTitle>What is Blue Card?</BCTitle>
+                    <BCInfoP>Welcome to Bluecard -- Your FREE Virtual Discount Program for all UBC Science Students! 
                             Discover exclusive benefits at local restaurants and shops by simply presenting your Blue Card.
-                            To get started, follow the register link at the top of the page. 
-                        </InfoP>
-                        <InfoP>
+                            To get started, follow the register link at the top of the page. <br /> <br />
                             Submit specific requests for places you want to enjoy discounts using the feedback function at the bottom of our page! 
                             Our SUS student benefits team will do our best to secure the desired discount for you.
-                        </InfoP>
-                    </Left>
-                    <Right>
-                        <ImgGradientRight>
-                            <BCInfoImgRight src="/images/index-images/main2.png" alt="Logo" />
-                        </ImgGradientRight>
-                    </Right>
-                </BCInfo>
+                    </BCInfoP>                           
+                </ImgSec>
                 <StyledLink id="eligibility"></StyledLink>
-                <Eligibility>
-                    <Left>
-                        <ImgGradientLeft>
-                            <BCInfoImgLeft src="/images/index-images/main2.png" alt="Logo" />
-                        </ImgGradientLeft>
-                    </Left>
-                    <Right>
-                        <Title>Eligibility</Title>
-                        <All>All UBC students in the Faculty of Science are eligible for the Blue Card.</All>
-                    </Right>
-                </Eligibility>
+                <ImgSec>
+                    <ImgBackground src="/images/bluecard-images/eligibility.jpg" alt="Logo" />
+                    <EligibilityTitle>Eligibility</EligibilityTitle>
+                    <InfoP>All UBC students in the Faculty of Science are eligible for the Blue Card.</InfoP>
+                </ImgSec>
                 <StyledLink id="partners"></StyledLink>
                 <Partners>
-                    <Title>Our Partners</Title>
+                    <PartnerTitle>Our Partners</PartnerTitle>
                     <PartnersBox>
                         <IndivPartnerInfo
                             partnerName="Arbutus Station Dental"
-                            logoSrc="/images/bluecard-images/arbutus station dental.png"
+                            logoSrc="/images/bluecard-images/partner-logos/arbutus station dental.png"
                             description="Free take-home teeth whitening kit with the completion 
                             of a new patient exam and cleaning."
                             location="Valid only at 2184 W Broadway #560, Vancouver, BC V6K 2E1"
                         />
                         <IndivPartner>
-                            <PartnerImgThin src="/images/bluecard-images/BAKD_Cookies_Logo.webp" /> 
+                            <PartnerImgThin src="/images/bluecard-images/partner-logos/BAKD_Cookies_Logo.webp" /> 
                             <PartnerNameThin>Bak'd</PartnerNameThin>
                             <Description>10% off</Description>
                         </IndivPartner>
                         <IndivPartnerInfo
                             partnerName="Chatime"
-                            logoSrc="/images/bluecard-images/chatime-logo.webp"
+                            logoSrc="/images/bluecard-images/partner-logos/chatime-logo.webp"
                             description="10% off"
                             location="Valid only at 5998 University Blvd #4, Vancouver, V6T 2A1"
                         />
                         <IndivPartnerInfo
                             partnerName="Delhi 6 Indian Bistro"
-                            logoSrc="/images/bluecard-images/delhi 6 indian bistro.png"
+                            logoSrc="/images/bluecard-images/partner-logos/delhi 6 indian bistro.png"
                             description="15% off"
                             location="Valid only at 1766 W 7th Ave"
                         />
                         <IndivPartner>
-                            <PartnerImgThin src="/images/bluecard-images/EYELAB.png" /> 
+                            <PartnerImgThin src="/images/bluecard-images/partner-logos/EYELAB.png" /> 
                             <PartnerNameThin>Eyelab, Doctors of Optometry</PartnerNameThin>
                             <Description>10% discount on contacts</Description>
                             <Location>Valid only at 2369 W 41st Ave, Vancouver, BC V6M 2A3</Location>
                         </IndivPartner>
                         <IndivPartnerInfo
                             partnerName="Gold's Gym"
-                            logoSrc="/images/bluecard-images/gold_s gym logo.png"
+                            logoSrc="/images/bluecard-images/partner-logos/gold_s gym logo.png"
                             description="35% off standard rate"
                             location="Valid only at 709 W Broadway, Vancouver, BC V5Z 1J5 2155 Allison Rd, Vancouver, BC V6T 1T5 1950 Oxford Connector, Port Coquitlam, BC V3B 4H3 19989 81a Ave, Langley Twp, BC V2Y 0C7"
                         />
                         <IndivPartnerInfo
                             partnerName="Hinbor"
-                            logoSrc="/images/bluecard-images/hinbor_logo.jpeg"
+                            logoSrc="/images/bluecard-images/partner-logos/hinbor_logo.jpeg"
                             description="Special weekly discounts for Science students!"
                             location="Email Rachel at info@hinbor.ca with your Hinbor app username to be included into exclusive science student discounts"
                         />
                         <IndivPartnerInfo
                             partnerName="Kung Fu Tea"
-                            logoSrc="/images/bluecard-images/KUNG FU TEA [BLACK].png"
+                            logoSrc="/images/bluecard-images/partner-logos/KUNG FU TEA [BLACK].png"
                             description="15% discount on all items"
                             location="Valid only at 2855 W Broadway, Vancouver, BC V6K 2G6"
                         />
                         <IndivPartnerInfo
                             partnerName="Majestic Maven"
-                            logoSrc="/images/bluecard-images/majestic-maven-14.jpg"
+                            logoSrc="/images/bluecard-images/partner-logos/majestic-maven-14.jpg"
                             description="25% off on all services for September then 20% for the rest of the year."
                             location=""
                         />
                         <IndivPartnerInfo
                             partnerName="Milano Casa Riistorante"
-                            logoSrc="/images/bluecard-images/milano casa logo.jpg"
+                            logoSrc="/images/bluecard-images/partner-logos/milano casa logo.jpg"
                             description="10% off on the dinner menu. Note: dinner menu starts from 6 pm"
                             location="Valid only at 4397 W 10th Ave, Vancouver, BC V6R 2H6"
                         />
                         <IndivPartnerInfo
                             partnerName="Pita Pit"
-                            logoSrc="/images/bluecard-images/PitaPit_logo.jpg"
+                            logoSrc="/images/bluecard-images/partner-logos/PitaPit_logo.jpg"
                             description="5% off"
                             location="Valid only at 5717 Dalhousie Rd, Vancouver, BC V6T 2H9"
                         />
                         <IndivPartnerInfo
                             partnerName="Steve's Poke Bar"
-                            logoSrc="/images/bluecard-images/Steve_s Poke Bar Logo.png"
+                            logoSrc="/images/bluecard-images/partner-logos/Steve_s Poke Bar Logo.png"
                             description="$5 off"
                             location="Valid at www.stevespokebar.ca"
                         />
                         <IndivPartnerInfo
                             partnerName="Wizeprep"
-                            logoSrc="/images/bluecard-images/wizeprep logo.png"
+                            logoSrc="/images/bluecard-images/partner-logos/wizeprep logo.png"
                             description="15% discount on Study Pass + Live Prep sessions + MCAT"
                             location="Valid at wizeprep.com"
                         />
@@ -202,11 +187,44 @@ const Heading = styled.div`
 const Title = styled.div`
     font-size: 54px;
     font-weight: 700;
-    position: relative;
+    position: absolute;
     letter-spacing: 5px;
     text-transform: uppercase;
-    margin-top: 24px;
+    z-index: 100;
 `
+
+const BCTitle = styled(Title)  `
+    top: 15%;
+    padding: 2%;
+
+      @media (max-width: ${breakpoints.mobile}) {
+        width: 100%;
+        min-height: 100%;
+        top: 5%;
+    }
+`
+
+const PartnerTitle = styled(Title)`
+    position: relative;
+`
+
+const MainTitle = styled(Title)`
+    position: relative;
+    top: 20px;
+`
+
+const EligibilityTitle = styled(Title) `
+    font-size: 54px;
+    font-weight: 700;
+    position: absolute;
+    letter-spacing: 5px;
+    text-transform: uppercase;
+    z-index: 100;
+    top: 30%;
+    padding: 2%;
+    text-align: center;
+`
+
 const SubInfo = styled.div`
     font-size: 24px;
     margin-top: 20px;
@@ -223,27 +241,7 @@ const SecondaryMenu = styled.div`
     width: 100%;
 `
 
-const BCInfo = styled.div`
-    background-color: #e7edf7;
-    width: 96%;
-    height: auto;
-    margin: 2% 2% 0 2%;
-    padding: 24px;
-    border-style: solid;
-    border-radius: 20px;
-    border-width: 0;
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
-    flex-wrap: wrap;
-
-    @media (max-width: ${breakpoints.tablet}) {
-        padding: 12px;
-        flex-direction: column;
-    }
-`
-
-const Partners = styled(BCInfo)`
+const Partners = styled.div`
     margin: 0 2% 2% 2%;
     text-align: center;
     justify-content: center;
@@ -253,15 +251,25 @@ const Partners = styled(BCInfo)`
     background-color: white;
     border-color: #e7edf7;
     border-width: 10px;
+    width: 96%;
+    height: auto;
+    padding: 24px;
+    border-style: solid;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
 `
-const Eligibility = styled(BCInfo) `
-    margin: 0 2%;
+const ImgSec = styled.div `
     justify-content: center;
     align-items: center;
-
-    @media (max-width: ${breakpoints.mobile}) {
-        text-align: right;
-    }
+    margin: 2% 2%;
+    flex-direction: column;
+    gap: 5px;
+    position: relative;
+    width: 96%;
+    height: 500px;
 `
 
 const SmItem = styled.div`
@@ -269,18 +277,26 @@ const SmItem = styled.div`
     text-align: center;
 `
 
-const InfoP = styled.div `
-    font-size: 18px;
-    color: #222755;
-    text-align: left;
-    padding: 24px 0;
-`
-
-const All = styled.div`
+const InfoP = styled.div`
     font-size: 30px;
     letter-spacing: 3px;
     width: 100%;
     font-weight: 300; 
+    position: absolute;
+    top: 42%;
+    z-index: 100;
+    padding: 2%;    
+
+`
+
+const BCInfoP = styled(InfoP) `
+    top: 30%;
+    font-size: 25px;
+    text-align: left;
+
+    @media (max-width: ${breakpoints.mobile}) {
+        font-size: 20px;
+    }
 `
 
 const MainInfo = styled.div`
@@ -289,9 +305,12 @@ const MainInfo = styled.div`
     flex-direction: column;
     flex-wrap: wrap;
     background: white;
-    gap: 15px;
     justify-content: center;
     align-items: center;
+
+    @media (max-width: ${breakpoints.mobile}) {
+        gap: 50px;
+    }
 `
 
 const PartnersBox = styled.div`
@@ -318,21 +337,6 @@ const IndivPartner = styled.div`
     }
 `
 
-const Left = styled.div`
-    max-width: 50%;
-    align-items: center;
-    justify-content: center;
-
-    @media (max-width: ${breakpoints.mobile}) {
-        max-width: 100%;
-    }
-        @media (max-width: ${breakpoints.tablet}) {
-        max-width: 100%;
-    }
-`
-const Right = styled(Left)`
-`
-
 const Background = styled.div`
     height: 100%;
     background-size: cover;
@@ -341,24 +345,20 @@ const Background = styled.div`
     background-color: #222755;
 `
 // images
-const BCInfoImgRight = styled.img`
-  min-width: 50%;
-  height: auto;
+const ImgBackground = styled.img`
+  width: 100%;
+  height: 500px;
   position: relative;
-  z-index: 0;
   border-radius: 10px;
   display: block;
+  z-index: 2;
+    opacity: 35%;
+    object-fit: cover;
 
   @media (max-width: ${breakpoints.mobile}) {
         width: 100%;
     }
  `
-
- const BCInfoImgLeft = styled(BCInfoImgRight)`
- `
-const ImgGradientLeft = styled.div``
-const ImgGradientRight = styled(ImgGradientLeft)`
-    `
 
 const StyledButton = styled.div`
     display: inline-block;
