0b9f18d57b8cef23a232e6fa9e352831bb630922
sahibrao <sahibbbbbb15@gmail.com>
2024-08-26 21:37:32 -0700

    initial exec

---
+++ src/executiveInfo.js
@@ -0,0 +1,107 @@
+export const executiveInfo = [
+  {
+    name: "Kaitlin Law",
+    role: "President",
+    imagePath: "/images/execuetive-images/Kaitlin-Law.webp",
+    description: `Hello UBC Science! My name is Kaitlin (she/her) and I’m a 5th year in Biotechnology! I am honoured to serve as your 2024-2025 Science Undergraduate Society (SUS) 
+      President. As SUS President, I focus on supporting the executive team with their specific portfolios, 
+      chair the SUS Executive Committee, and oversee the Projects, EDI and HR Working Groups. I am also tasked with thinking about the big picture for SUS and guiding our overall efforts at various levels of the university. 
+      I hope to best represent our diverse Science students this year through events, advocacy, and change! \n \n If you are interested in learning more about SUS, 
+      have any ideas or suggestions, or would like to set up a coffee chat, please feel free to contact me via email at president@sus.ubc.ca.`,
+    email: "president@sus.ubc.ca",
+  },
+  {
+    name: "Thomas McIlwraith",
+    role: "VP Administration",
+    imagePath: "/images/execuetive-images/Thomas-McIlwraith.webp",
+    description: `Hey UBC Science! I’m Thomas, I’m in my 3rd year of Environmental Sciences, and I am so excited and honoured to be your VP Administration for this year! The admin portfolio helps run lots of behind-the-scenes work in SUS, including managing the Abdul Ladha Science Student Centre (ALSSC), organizing SUS documents and meeting minutes, promoting sustainability,
+      and supporting SUS Clubs. \n\nIf you have any questions, suggestions, or thoughts about anything above, including how to become a SUS club, SUS governance documents, meeting minutes, ideas for the ALSSC, sustainability initiatives, or anything else within the portfolio, please don’t hesitate to reach out! You can contact me at vpadministration@sus.ubc.ca.`,
+    email: "vpadministration@sus.ubc.ca",
+  },
+  {
+    name: "Christianah Soneye",
+    role: "VP Academic",
+    imagePath: "/images/execuetive-images/Christianah-Soneye.webp",
+    description: `Hi UBC Science! My name is Christianah Soneye (she/her), and I’m a fifth-year Integrated Science student (Human Physiology & Public Health). I am honoured to serve as your 2023-2024 Science Undergraduate Society (SUS) Vice-President Academic.
+    \nAs the SUS VP Academic, I am here to help ensure that you get the most out of your time here at UBC Science by enhancing your academic experience, offering mentorship opportunities, and promoting physical and mental wellness outside of the lecture hall. I also have the privilege of representing you at the Faculty level, advocating for your concerns, sharing the student perspective, and supporting accessibility and inclusion on the Skylight Development Grants, Teaching and Learning Enhancement Fund (TLEF), Science Curriculum, Dean Advisory and Killam Teaching Awards Committees.
+    \nIf you have any concerns about your academic experiences, suggestions for points of advocacy at the Faculty level, or thoughts on how SUS could further support your academic, physical and mental wellbeing, please feel free to reach out to me via email at vpacademic@sus.ubc.ca.`,
+    email: "vpacademic@sus.ubc.ca",
+  },
+  {
+    name: "Insha Majeed",
+    role: "VP Communications",
+    imagePath: "/images/execuetive-images/Insha-Majeed.webp",
+    description: `Hi UBC Science! My name is Insha Majeed (she/her), and I am a third year in Microbiology and Immunology. It is both an honour and a privilege to serve you as your VP Communications for the 2024/25 school year! As VP Communications, I oversee and support the following working groups: Marketing, Productions, Sales, Visual media, First Week and Webmasters.
+      \nTogether with my team, I strive to ensure that SUS has an impactful and inclusive presence across all communication platforms. Our goal is to provide you with relevant information promptly, fostering a culture of accessibility, creativity, and engagement within the science student body.
+      \nIf you have any questions about the communications portfolio, including event marketing, videography/photography, or ideas for SUS social media platforms and the website, feel free to reach out to me at vpcommunications@sus.ubc.ca. I’m here to help!
+      `,
+    email: "vpcommunications@sus.ubc.ca",
+  },
+  {
+    name: "Rachel Dong",
+    role: "VP External",
+    imagePath: "/images/execuetive-images/Rachel-Dong.webp",
+    description: ` Hi UBC Science! My name is Rachel (she/her) and I’m a 3rd year majoring in Integrated Sciences! I am honoured to serve as your 2024-2025 Science Undergraduate Society (SUS) Vice President External. 
+    \nAs VP External, I oversee and support the following working groups: Careers & Professional Development, Community Engagement, Corporate Relations, and Student Benefits. Our portfolio collaborates with external organizations to connect science students to career development and volunteer opportunities, raise awareness of humanitarian causes, obtain sponsorships for SUS portfolios, and provide discounts through the Blue Card program.  
+    \nIf you are interested in learning more about the External Portfolio, have any ideas or suggestions, or would like to partner with SUS, please feel free to contact me at vpexternal@sus.ubc.ca. 
+    `,
+    email: "vpexternal@sus.ubc.ca",
+  },
+  {
+    name: "Chloe Chan",
+    role: "VP Finance",
+    imagePath: "/images/execuetive-images/Chloe-Chan.webp",
+    description: `Hi UBC Science! My name is Chloe Chan (she/her) and I am a fifth year Biotechnology student. I am super excited to be serving as your VP Finance for the upcoming year! 
+    \nThe Finance Portfolio provides financial support to a variety of students and clubs across campus. This year, I’m hoping to bring the Finance portfolio closer to the students by holding some financial literacy events, as well as by increasing budget transparency. I also hope to increase grant accessibility by holding grant application writing workshops, so make sure to keep your eyes peeled for updates on our socials!
+    \nIf you have any questions or concerns, or if you have any ideas for some financial literacy workshops, or even if you just wanna chat — please feel free to reach out to me via vpfinance@sus.ubc.ca!
+     `,
+    email: "vpfinance@sus.ubc.ca",
+  },
+  {
+    name: "Chloe Chang",
+    role: "VP Internal",
+    imagePath: "/images/execuetive-images/Chloe-Chang.webp",
+    description: `Hey UBC Science! My name is Chloe Chang (she/her) and I am a 3rd-year Integrated Science student specializing in Immunology, Public Health, and Environmental Sciences. I am honored to be serving as your Vice President Internal for this academic year. The Internal Portfolio relates to anything and everything that concerns the inner workings of SUS and here are the following committees within the portfolio: Internal Committee, Elections Administration, First Year Committee and Science Student Recognition Awards.
+    \nWith the help of my team, I plan to improve community engagement, expand the impact of the Science Student Recognition Awards Night and improve inter and intra-portfolio relations. I hope that student’s feel welcomed and appreciated when they join the team or get involved with SUS.
+    \nIf you want to learn more about running for Elections or have general questions about SUS, feel free to contact me via email (vpinternal@sus.ubc.ca).
+    `,
+    email: "vpinternal@sus.ubc.ca",
+  },
+  {
+    name: "Sarveen Sohi",
+    role: "VP Student Life",
+    imagePath: "/images/execuetive-images/Sarveen-Sohi.webp",
+    description: `Hello UBC Science! I am Savreen Sohi (she/her) and I am currently a fifth-year Integrated Science student. I am excited to be your VP Student Life for the 2024/2025 school year!
+    \nThe Student Life Portfolio is responsible for flagship events such as Science RXN, Science Week, and Science Celebration (formerly known as Science Graduation), as well as the Social and Sports Working Group events throughout the school year. The Student Life Portfolio aims to plan and execute events that get the student body excited to participate and are inclusive towards everyone.
+    \nFeel free to reach out to me at vpstudentlife@sus.ubc.ca to share event ideas, to learn more about SUS and Student Life, or answer any questions you may have!
+    `,
+    email: "vpstudentlife@sus.ubc.ca",
+  },
+  {
+    name: "Salva Sherif",
+    role: "Science Senator",
+    imagePath: "/images/execuetive-images/Salva-Sherif.webp",
+    description: `Hey! I’m Salva, your Science Senator for the year. I’m super excited to represent our amazing Science community at UBC! My mission is to make UBC Senate and our university’s general academic direction more representative of what students in our Faculty need. Whether you’re facing academic policy-related challenges or have specific priorities you would like me to advocate for, I’m here to help. Let’s work together to make this year incredible.
+    \nFeel free to reach out anytime at senator@sus.ubc.ca :))`,
+    email: "senator@sus.ubc.ca",
+  },
+  {
+    name: "Vedin Schimmack",
+    role: "Science Representative",
+    imagePath: "/images/execuetive-images/Vedin-Schimmack.webp",
+    description: `Hi, my name is Vedin (any/all)! I’m a 3rd year Combined Major in Computer Science and Biology student who’s also doing Science Co-op. Currently, I’m one of the Science Representatives to AMS Council as well as a coordinator and treasurer for Climate Justice UBC. I have a history and strong passion for environmental and social justice advocacy and am hoping to improve sustainability in the AMS.
+    \nFeel free to message me anytime at vedinrs.ubc@gmail.com or on instagram at vedin4ams about any ideas you may have for SUS or the AMS or any questions (especially ones on how to get involved)!
+    `,
+    email: "vedinrs.ubc@gmail.com",
+  },
+  {
+    name: "Yuki Ichikawa",
+    role: "Science Representative",
+    imagePath: "/images/execuetive-images/Yuki-Ichikawa.webp",
+    description: `Hi UBC Science! My name is Yuki Ichikawa (she/her) and I’m in my 4th year in the Cellular, Anatomical and Physiological Sciences. I am excited and honoured to be serving as one of the AMS representatives for the 2024/2025 year!
+    \nAs an AMS representative it is my primary responsibility to actively advocate and work for all science students on AMS council and committees. AMS council is the highest decision making body in the AMS. It is crucial that our diverse experiences and needs are reflected in all the decisions that the AMS makes, how our student fees are spent, and how these decisions might impact our time at UBC. As such, I would love to hear from you! Whether it’s questions about how the AMS works, ideas and opinions you think are underrepresented at the AMS, or concerns around specific decisions being made, you can reach myself and the other AMS representatives at amsrep@sus.ubc.ca.
+    \nThis summer I will be serving on the AMS Operations and HR committees, more informationabout these committees and be found at ams.ubc.ca.
+    `,
+    email: "amsrep@sus.ubc.ca",
+  },
+];
--- src/pages/executives.js
+++ src/pages/executives.js
@@ -1,44 +1,170 @@
+import { useState } from "react";
 import styled from "styled-components";
-import Link from 'next/link';
-
-export default function Executives() {
-    return (
-        <Background>
-            <Title>Executives Information page</Title>
-            <Link href="/" passHref>
-                <StyledButton>Back to Home</StyledButton>
-            </Link>
-        </Background>
-    )
-}
-
-const Title = styled.div`
-    color: black;
-`
+import Navbar from "../../components/Navbar";
+import { Modal } from "../../components/index-components/Modal";
+import { executiveInfo } from "../executiveInfo";
 
 const Background = styled.div`
-    height: 100vh;
-    background-size: cover;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 200px;
+  padding-bottom: 200px;
+  padding-left: 10%;
+  padding-right: 10%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  gap: 20px;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 30px;
+`;
+
+const Header = styled.div`
+  font-size: 37px;
+  font-weight: bold;
+  color: #001961;
+`;
+
+const SubHeader = styled.div`
+  font-size: 20px;
+  color: #001961;
+  width: 60%;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+// need this to contain all the execuetives
+// should be a grid
+// need to make this dynamic
+const ExecuetiveContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+
+  @media only screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media only screen and (max-width: 768px) {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-`
+  }
+`;
 
-const StyledButton = styled.div`
-    display: inline-block;
-    width: 210px;
-    padding: 10px 20px;
-    background-color: #0070f3;
-    color: white;
-    text-align: center;
-    border-radius: 5px;
-    text-decoration: none;
-    margin-top: 20px;
+// need this to the circle that contains the imgae
+const ExecutiveCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 350px;
+  gap: 20px;
+
+  &:hover {
     cursor: pointer;
+  }
+`;
 
-    &:hover {
-        background-color: #005bb5;
-    }
-`
+const ExecutiveTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const ExecutiveTitle = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const ExecutiveName = styled.div`
+  font-size: 16px;
+`;
+
+const ExecutiveImage = styled.img`
+  overflow: hidden;
+  border-radius: 50%;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  width: 200px;
+  height: 200px;
+`;
+
+export const Executive = ({ name, role, imagePath, email, description }) => {
+  const [modalClose, setModalClose] = useState(true);
+
+  const setCloseModal = () => {
+    setModalClose(!modalClose);
+  };
+
+  return (
+    <>
+      <Modal
+        modalClose={modalClose}
+        setCloseModal={setCloseModal}
+        name={name}
+        role={role}
+        imagePath={imagePath}
+        email={email}
+        description={description}
+      />
+      <ExecutiveCard onClick={setCloseModal}>
+        <ExecutiveImage src={imagePath} />
+        <ExecutiveTextContainer>
+          <ExecutiveTitle>{role}</ExecutiveTitle>
+          <ExecutiveName>{name}</ExecutiveName>
+        </ExecutiveTextContainer>
+      </ExecutiveCard>
+    </>
+  );
+};
+
+export default function Executives() {
+  return (
+    <Background>
+      <Navbar />
+      <Content>
+        <HeaderContainer>
+          <Header>Meet the Executives</Header>
+          <SubHeader>
+            Here you can find the contact information of each executive to be
+            able to contact them. As well, office hours are held for each
+            executive, and you can attend them at Abdul Ladha. 
+          </SubHeader>
+        </HeaderContainer>
+        <ExecuetiveContainer>
+          {executiveInfo.map((executive) => {
+            return (
+              <Executive
+                name={executive.name}
+                role={executive.role}
+                email={executive.email}
+                description={executive.description}
+                imagePath={executive.imagePath}
+              />
+            );
+          })}
+        </ExecuetiveContainer>
+      </Content>
+    </Background>
+  );
+}
