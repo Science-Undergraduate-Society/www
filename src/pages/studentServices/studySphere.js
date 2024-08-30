34ae12a443c7887de43052d9c2569fe70d3e2a0c
abbyg85 <abbyg85@student.ubc.ca>
2024-08-30 16:37:03 -0700

    basic structure for study sphere page

--- src/pages/studentServices/studySphere.js
+++ src/pages/studentServices/studySphere.js
@@ -1,19 +1,59 @@
 import styled from "styled-components";
 import Link from 'next/link';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function StudySphere() {
     return (
         <Background>
-            <Title>Study Sphere</Title>
-            <Link href="/" passHref>
-                <StyledButton>Back to Home</StyledButton>
-            </Link>
+            <Navbar />
+            <Content>
+                <Title>Study Sphere</Title>
+                <SubTitle>Our new notes sharing platform!</SubTitle>
+                <p>Share and use course specific study materials with your peers.</p>
+                <Buttons>
+                    <Link href="https://ubcca-my.sharepoint.com/:f:/g/personal/njpv02_student_ubc_ca/ElDWP5sexdVFs1c6Hdzit2oBu9tFuMKhKGQqWSV8kdpVtQ?e=EFv22E" passHref>
+                    <StyledButton>Share Your Notes</StyledButton>
+                    </Link>
+                    <Link href="https://ubcca-my.sharepoint.com/:f:/g/personal/njpv02_student_ubc_ca/EgiE8oZ-X65JgP65CyeH350BzhtYdmB1PDIOuYDvwBgIUQ?e=QaTh2V" passHref>
+                    <StyledButton>View Shared Notes</StyledButton>
+                    </Link>
+                </Buttons>
+            </Content>
+            <Footer />
         </Background>
     )
 }
 
 const Title = styled.div`
-    color: black;
+    padding-top: 24px;
+    font-size: 54px;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+`
+
+const SubTitle = styled.div`
+    font-size: 24px;
+    letter-spacing: 4px;
+`
+
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 10px;
+`
+
+const Content = styled.div `
+  color: #222755;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  text-align: left;
+  width: 100%;
+  padding: 150px 0 0 15%;
 `
 
 const Background = styled.div`
@@ -21,9 +61,7 @@ const Background = styled.div`
     background-size: cover;
     display: flex;
     flex-direction: column;
-    align-items: center;
     justify-content: center;
-    text-align: center;
 `
 
 const StyledButton = styled.div`
