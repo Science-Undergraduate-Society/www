fa2134e808bb7fef3be1b74e1d530ad09dfcfdc4
davidlim <daviddlim11@gmail.com>
2024-09-14 19:36:35 -0700

    web configuration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,66 @@
 import '../styles/globals.css'; // Import the global CSS file
 import Head from 'next/head';
 
-export default function MyApp ({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>UBC Science Undergraduate Society</title>
+        <title>Science Undergraduate Society</title>
         <meta
           name="description"
           content="The Science Undergraduate Society is a student-led organization for science students at the University of British Columbia in Vancouver."
         />
-        <meta property="og:title" content="UBC Science Undergraduate Society" key="ogtitle" />
-        <meta
-          property="og:description"
-          content="The Science Undergraduate Society is a student-led organization for science students at the University of British Columbia in Vancouver."
-          key="ogdescription"  ddid  tr
-          
+        <meta 
+          name="keywords" 
+          content="SUS UBC, Science Undergraduate Society, UBC Science, Science Society, UBC Student Society, UBC Vancouver, Student Organization"
         />
-        {/* <meta property="og:image" content="/path/to/your/image.jpg" key="ogimage" /> */}
-        {/* <meta property="og:url" content="https://www.sus.ubc.ca" key="ogurl" /> */}
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <meta 
+          property="og:title" 
+          content="Science Undergraduate Society | University of British Columbia" 
+          key="ogtitle" 
+        />
+        <meta 
+          property="og:description" 
+          content="The Science Undergraduate Society (SUS) is the official representative body for science students at UBC. Learn about events, resources, and ways to get involved."
+          key="ogdescription" 
+        />
+        <meta property="og:image" content="/faviconBlue.png" key="ogimage" />
+        <meta property="og:url" content="https://www.susubc.org" key="ogurl" />
+        <meta property="og:type" content="website" key="ogtype" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Science Undergraduate Society | University of British Columbia" />
+        <meta 
+          name="twitter:description" 
+          content="The Science Undergraduate Society (SUS) at UBC represents science students, offering resources, events, and opportunities to get involved."
+        />
+        <meta name="twitter:image" content="/faviconBlue.png" />
+        <meta name="twitter:url" content="https://www.susubc.org" />
+
         <link rel="icon" href="/faviconBlue.png" />
+        <link rel="canonical" href="https://www.susubc.org" />
+
+        <script type="application/ld+json">
+        {{
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "Science Undergraduate Society",
+          "url": "https://www.susubc.org",
+          "logo": "https://www.susubc.org/faviconBlue.png",
+          "sameAs": [
+            "https://www.facebook.com/susubc",
+            "https://www.instagram.com/susubc"
+          ],
+          "contactPoint": {
+            "@type": "ContactPoint",
+            "telephone": "+1-604-822-4235",
+            "contactType": "Abdul Ladha Front Desk"
+          }
+        }}
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
