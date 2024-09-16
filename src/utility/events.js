07d926e7a641089df9f53122bd1b1e9f32c85d8e
davidlim <daviddlim11@gmail.com>
2024-09-15 19:48:17 -0700

    event changes

--- src/utility/events.js
+++ src/utility/events.js
@@ -1,50 +1,50 @@
 const eventsArray = 
   [
-    {
-      title: "Faculty Fair / Imagine Day", 
-      date: "September 3, 2024",
-      time: "10am - 4pm", 
-      location: "Front of Abdul Ladha",
-      description: "Kick off the new school year at UBC by exploring the Faculty Fair at Lee Square! Dive into academic and extracurricular opportunities, connect with faculty, and discover clubs that match your interests. Enjoy free merch, spin for prizes, and proudly wear your UBC Science gear as you connect with fellow students and build lasting relationships.",
-      image: "/images/events-images/imagine-day.jpeg",
-      link: "https://www.instagram.com/susubc/"
-    },
-    {
-      title: "Bunny Therapy", 
-      date: "September 4, 2024",
-      time: "1pm - 3pm", 
-      location: "Abdul Ladha",
-      description: "Ease into the new school year with Bunny Therapy! Relax, reduce stress, and lift your spirits with the help of adorable, fluffy bunnies!",
-      image: "/images/events-images/bunny-therapy.png",
-      link: "https://www.instagram.com/p/C_OdMifSqwW/"
-    },
-    {
-      title: "Mocktail Paint and Sip",
-      date: "September 4, 2024",
-      time: "6pm - 8pm",
-      location: "Abdul Ladha",
-      description: "Start the school year creatively at Mocktail Paint and Sip! Enjoy painting, sipping mocktails, and connecting with fellow students in a relaxed setting.",
-      image: "/images/events-images/mocktail-paint-sip.jpeg",
-      link: "https://www.instagram.com/p/C_OdMifSqwW/"
-    },
-    {
-      title: "Outdoor Movie Night",
-      date: "September 5, 2024",
-      time: "7:30pm - 10pm",
-      location: "Abdul Ladha",
-      description: "We're brining back Outdoor Movie Night! Bring your blanket, and enjoy a film along with some free eats!",
-      image: "/images/events-images/movie-night.jpeg",
-      link: "https://www.instagram.com/p/C_OdMifSqwW/"
-    },
-    {
-      title: "Welcome Back BBQ",
-      date: "September 6, 2024",
-      time: "5pm - 8pm",
-      location: "Front of Abdul Ladha",
-      description: "Wrap up the first week of school with our Welcome Back BBQ! Join first-year and returning students for food, fun, and a dunk tank as you connect and relax. (p.s. we're dunking professors)",
-      image: "/images/events-images/welcome-back-bbq.jpg",
-      link: "https://www.instagram.com/p/C_OdMifSqwW/"
-    },
+    // {
+    //   title: "Faculty Fair / Imagine Day", 
+    //   date: "September 3, 2024",
+    //   time: "10am - 4pm", 
+    //   location: "Front of Abdul Ladha",
+    //   description: "Kick off the new school year at UBC by exploring the Faculty Fair at Lee Square! Dive into academic and extracurricular opportunities, connect with faculty, and discover clubs that match your interests. Enjoy free merch, spin for prizes, and proudly wear your UBC Science gear as you connect with fellow students and build lasting relationships.",
+    //   image: "/images/events-images/imagine-day.jpeg",
+    //   link: "https://www.instagram.com/susubc/"
+    // },
+    // {
+    //   title: "Bunny Therapy", 
+    //   date: "September 4, 2024",
+    //   time: "1pm - 3pm", 
+    //   location: "Abdul Ladha",
+    //   description: "Ease into the new school year with Bunny Therapy! Relax, reduce stress, and lift your spirits with the help of adorable, fluffy bunnies!",
+    //   image: "/images/events-images/bunny-therapy.png",
+    //   link: "https://www.instagram.com/p/C_OdMifSqwW/"
+    // },
+    // {
+    //   title: "Mocktail Paint and Sip",
+    //   date: "September 4, 2024",
+    //   time: "6pm - 8pm",
+    //   location: "Abdul Ladha",
+    //   description: "Start the school year creatively at Mocktail Paint and Sip! Enjoy painting, sipping mocktails, and connecting with fellow students in a relaxed setting.",
+    //   image: "/images/events-images/mocktail-paint-sip.jpeg",
+    //   link: "https://www.instagram.com/p/C_OdMifSqwW/"
+    // },
+    // {
+    //   title: "Outdoor Movie Night",
+    //   date: "September 5, 2024",
+    //   time: "7:30pm - 10pm",
+    //   location: "Abdul Ladha",
+    //   description: "We're brining back Outdoor Movie Night! Bring your blanket, and enjoy a film along with some free eats!",
+    //   image: "/images/events-images/movie-night.jpeg",
+    //   link: "https://www.instagram.com/p/C_OdMifSqwW/"
+    // },
+    // {
+    //   title: "Welcome Back BBQ",
+    //   date: "September 6, 2024",
+    //   time: "5pm - 8pm",
+    //   location: "Front of Abdul Ladha",
+    //   description: "Wrap up the first week of school with our Welcome Back BBQ! Join first-year and returning students for food, fun, and a dunk tank as you connect and relax. (p.s. we're dunking professors)",
+    //   image: "/images/events-images/welcome-back-bbq.jpg",
+    //   link: "https://www.instagram.com/p/C_OdMifSqwW/"
+    // },
     {
       title: "Science RXN", 
       date: "September 14, 2024 - September 15, 2024",
