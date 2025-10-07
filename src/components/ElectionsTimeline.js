c5d81f8dd6a0f4c3c426552a82ad6d99c2790eb0
David Lim <134044460+dvidEast@users.noreply.github.com>
2025-10-06 17:48:48 -0700

    Update ElectionsTimeline.js

--- src/components/ElectionsTimeline.js
+++ src/components/ElectionsTimeline.js
@@ -39,53 +39,58 @@ export default function ElectionsTimeline() {
   return (
     <Timeline>
       <TimelineItem>
-        <DateText>Friday, Sept 5th, 12:00 AM</DateText>
+        <DateText>Friday, Sept 5, 12:00 AM</DateText>
         <EventText>Nominations open with a Qualtrics survey</EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Friday, Sept 19th, 11:59 PM</DateText>
+        <DateText>Friday, Sept 19, 11:59 PM</DateText>
         <EventText>Deadline for submission of completed nomination submissions</EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Wednesday, Sept 24th, 8:00 PM</DateText>
+        <DateText>Wednesday, Sept 24, 8:00 PM</DateText>
         <EventText>
           <strong>MANDATORY</strong> All Candidates Meeting (via Zoom), time will depend on availability of candidates
         </EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Friday, Sept 26th, 11:59 PM</DateText>
+        <DateText>Friday, Sept 26, 11:59 PM</DateText>
         <EventText>
           Deadline for sending in headshots & blurbs for voting platform to elections@sus.ubc.ca
         </EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Monday, Sept 29th, 12:00 AM</DateText>
+        <DateText>Monday, Sept 29, 12:00 AM</DateText>
         <EventText>Campaigning begins (paperless campaigning)</EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Monday, Oct 6th, 12:00 AM</DateText>
+        <DateText>Monday, Oct 6, 12:00 AM</DateText>
         <EventText>
           Voting opens at <a href="https://ams.simplyvoting.com" target="_blank" rel="noopener noreferrer">ams.simplyvoting.com</a>
         </EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Wednesday, Oct 8th, 6:00 PM</DateText>
+        <DateText>Wednesday, Oct 8, 6:00 PM</DateText>
         <EventText>All Candidates Forum for VP Academic, VP Finance, and AMS Reps</EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Monday, Oct 13th, 11:59 PM</DateText>
+        <DateText>Monday, Oct 13, 11:59 PM</DateText>
         <EventText>Campaigning ends AND Voting closes</EventText>
       </TimelineItem>
 
       <TimelineItem>
-        <DateText>Thursday, Oct 16th, 5:00 PM</DateText>
+        <DateText>Thursday, Oct 23, 5:00 PM</DateText>
+        <EventText>Councillor Orientation (in-person) – 5-7 PM</EventText>
+      </TimelineItem>
+      
+      <TimelineItem>
+        <DateText>Thursday, Nov 27, 5:00 PM</DateText>
         <EventText>Councillor Orientation (in-person) – 5-7 PM</EventText>
       </TimelineItem>
     </Timeline>
