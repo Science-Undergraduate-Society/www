cfa7c5e585fd2720bd0d5dd32d9157580465bc1c
Claire Zhang <clairezhang838@gmail.com>
2025-11-17 10:41:21 -0800

    fix

--- src/pages/ourEvents/upcoming.js
+++ src/pages/ourEvents/upcoming.js
@@ -202,7 +202,7 @@ export default function Events() {
                 <EventContainer>
                     <EventTitle><b>Ignite <Highlight>Conference</Highlight></b></EventTitle>
                     <EventSubtitle><strong>November 22, 1:00 - 6:00 PM</strong></EventSubtitle>
-                    <p>Ignite is the Science Undergraduate Society's annual career conference, offering students interactive workshops, industry panels, and networking opportunities with professionals. Join us on November 22 to explore diverse science careers and gain insights to shape your professional journey.</p>
+                    <p>Ignite is the Science Undergraduate Society`&apos;`s annual career conference, offering students interactive workshops, industry panels, and networking opportunities with professionals. Join us on November 22 to explore diverse science careers and gain insights to shape your professional journey.</p>
                 
                     <a href="https://www.instagram.com/susubc/?hl=en" target="_blank" rel="noopener noreferrer">
                         <InitiativeButton><strong>Find out more</strong></InitiativeButton>
